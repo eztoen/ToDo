d8e6ec619d8b0002d492720271c458834365f9e1
eztoen <ikucheren0510@gmail.com>
2025-11-05 21:03:09 +0600

    Created UserCreate schema(username, email, password) with password validation

---
+++ auth/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, EmailStr, field_validator
+import re
+
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=5, max_length=35, pattern=r'^[a-zA-Z0-9_]+$')
+    email: EmailStr = Field(..., min_length=5, max_length=256)
+    password: str = Field(..., min_length=8, max_length=128)
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Password must contain at least one special character.")
+        if " " in value:
+            raise ValueError("Password cannot contain spaces.")
+        return value
