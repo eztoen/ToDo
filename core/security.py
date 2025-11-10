5d30c296ffb1ec5ee127b9ee28c36a2c65adbb01
eztoen <ikucheren0510@gmail.com>
2025-11-10 21:00:34 +0600

    Installed 'python-jose' and created class 'Security' with some functions for hashing and verify password and create JWT token

---
+++ core/security.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from typing import Optional
+from .config import settings
+
+pwd_context = CryptContext(schemes=['bcrypt'])
+
+class Security(BaseSettings):
+    SECRET_KEY = str
+    ALGORITHM = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+    
+    def get_password_hash(password):
+        return pwd_context.hash(password)
+    
+    def verify_password(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
+    def create_access_token(self, data: dict, expire_delta: Optional[timedelta] = None):
+        to_encode = data.copy()
+        expire = datetime.datetime.now(datetime.timezone.utc) + (expire_delta or timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES))
+        to_encode.update({'exp': expire})
+        return jwt.encode(
+            to_encode, 
+            key=self.SECRET_KEY, 
+            algorithm=self.ALGORITHM
+        )
+        
+    model_config = SettingsConfigDict(env_file='.env')
