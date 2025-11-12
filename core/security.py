5dc392abfbe1657ae65d2398484a27100751eb0a
eztoen <ikucheren0510@gmail.com>
2025-11-12 20:44:10 +0600

    Added type hints for functions: get_password_hash, verify_password

--- core/security.py
+++ core/security.py
@@ -5,17 +5,17 @@ from passlib.context import CryptContext
 from typing import Optional
 from .config import settings
 
-pwd_context = CryptContext(schemes=['bcrypt', 'argon2'])
+pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
 
 class Security(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     
-    def get_password_hash(self, password):
+    def get_password_hash(self, password: str) -> str:
         return pwd_context.hash(password)
     
-    def verify_password(plain_password, hashed_password):
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
         return pwd_context.verify(plain_password, hashed_password)
 
     def create_access_token(self, data: dict, expire_delta: Optional[timedelta] = None):
