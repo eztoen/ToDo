71f07f001fb6b37c8e9167cd867de07a131d0ecd
eztoen <ikucheren0510@gmail.com>
2025-11-17 16:28:17 +0600

    Added rate limiting to task endpoints with custom limits per endpoint to avoid brute force and DDoS attacks
    - POST /register:  10 requests/5min
    - POST /login: 10 requests/10min

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import crud
 from .schemas import UserRegister, UserLogin, TokenResponse
-from core.models import get_db
+from core.models import get_db, rate_limiter
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
 @router.post('/register', response_model=TokenResponse)
-async def register_user(new_user: UserRegister, session: AsyncSession = Depends(get_db)):
+@rate_limiter(limit=10, period=300)
+async def register_user(request: Request, new_user: UserRegister, session: AsyncSession = Depends(get_db)):
     return await crud.register_user(new_user=new_user, session=session)
 
 @router.post('/login', response_model=TokenResponse)
-async def login_user(user_data: UserLogin, session: AsyncSession = Depends(get_db)):
+@rate_limiter(limit=10, period=600)
+async def login_user(request: Request, user_data: UserLogin, session: AsyncSession = Depends(get_db)):
     return await crud.login_user(user_data=user_data, session=session)
